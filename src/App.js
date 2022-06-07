45d8b87907577d6bf612aeca110d36114e6fa506
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-06-07 19:00:48 -0300

    Req. 02: implementado state, função de setter e values para o filtro via nome

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Context from './context/Context';
 import fetchPlanets from './helpers/FetchAPI';
+import FilterByName from './components/FilterByName';
 import Table from './components/Table';
 
 function App() {
   const [data, setData] = useState(null);
+  const [filterByName, setFilterByName] = useState({ name: '' });
 
   useEffect(() => {
     const requestApi = async () => {
@@ -15,10 +17,15 @@ function App() {
     requestApi();
   }, []);
 
+  const searchPlanetByName = (e) => {
+    setFilterByName({ name: e.target.value });
+  };
+
   return (
     <>
       <h1>Star Wars</h1>
-      <Context.Provider value={ data }>
+      <Context.Provider value={ { data, filterByName, searchPlanetByName } }>
+        <FilterByName />
         <Table />
       </Context.Provider>
     </>
