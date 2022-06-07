685bc9acd0db1d9888b4be78c7022df37d90821d
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-06-07 19:34:00 -0300

    Req. 02: implementada a lógica do filtro pelo nome no componente Table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,62 +1,49 @@
 import React, { useContext } from 'react';
 import Context from '../context/Context';
 import Loading from '../img/loading.gif';
+import TableHeader from './TableHeader';
 
 function Table() {
-  const data = useContext(Context);
+  const { data, filterByName } = useContext(Context);
 
   return (
     !data ? <img src={ Loading } alt="loading..." />
       : (
         <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Rotation Period</th>
-              <th>Orbital Period</th>
-              <th>Diameter</th>
-              <th>Climate</th>
-              <th>Gravity</th>
-              <th>Terrain</th>
-              <th>Surface Water</th>
-              <th>Population</th>
-              <th>Films</th>
-              <th>Created</th>
-              <th>Edited</th>
-              <th>URL</th>
-            </tr>
-          </thead>
+          <TableHeader />
           <tbody>
-            {data.map(({
-              name,
-              rotation_period: rotationPeriod,
-              orbital_period: orbitalPeriod,
-              diameter,
-              climate,
-              gravity,
-              terrain,
-              surface_water: surfaceWater,
-              population,
-              films,
-              created,
-              edited,
-              url },
-            index) => (
-              <tr key={ index }>
-                <td>{name}</td>
-                <td>{rotationPeriod}</td>
-                <td>{orbitalPeriod}</td>
-                <td>{diameter}</td>
-                <td>{climate}</td>
-                <td>{gravity}</td>
-                <td>{terrain}</td>
-                <td>{surfaceWater}</td>
-                <td>{population}</td>
-                <td>{films}</td>
-                <td>{created}</td>
-                <td>{edited}</td>
-                <td>{url}</td>
-              </tr>))}
+            {data.filter(({ name }) => name
+              .includes(filterByName.name))
+              .map(({
+                name,
+                rotation_period: rotationPeriod,
+                orbital_period: orbitalPeriod,
+                diameter,
+                climate,
+                gravity,
+                terrain,
+                surface_water: surfaceWater,
+                population,
+                films,
+                created,
+                edited,
+                url },
+              index) => (
+                <tr key={ index }>
+                  <td>{name}</td>
+                  <td>{rotationPeriod}</td>
+                  <td>{orbitalPeriod}</td>
+                  <td>{diameter}</td>
+                  <td>{climate}</td>
+                  <td>{gravity}</td>
+                  <td>{terrain}</td>
+                  <td>{surfaceWater}</td>
+                  <td>{population}</td>
+                  <td>{films}</td>
+                  <td>{created}</td>
+                  <td>{edited}</td>
+                  <td>{url}</td>
+                </tr>))}
           </tbody>
         </table>
       )
