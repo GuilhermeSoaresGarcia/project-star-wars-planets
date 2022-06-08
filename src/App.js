05bad99a3f3e77c28143aed7f147a9d871721ed6
Guilherme Soares Garcia <guilherme.garcia@email.com>
2022-06-08 19:43:20 -0300

    Req. 03: refatoração

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ import FilterByNumber from './components/FilterByNumbers';
 import Table from './components/Table';
 
 function App() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
   const [filterByName, setFilterByName] = useState({ name: '' });
   const [numericFilterParameters, setNumericFilterParameters] = useState({
     column: '',
@@ -21,9 +22,43 @@ function App() {
     const requestApi = async () => {
       const planetsRequest = await fetchPlanets(); // Talvez não valha a pena componentizar o fetch como fiz aqui, já que a quantidade de linhas pra fazer funcionar é quase igual a fazer o fetch direto no useEffect
       setData(planetsRequest);
+      setFilteredData(planetsRequest);
     };
     requestApi();
   }, []);
+
+  useEffect(() => { // Lógica para a filtragem por nome
+    setFilteredData(data.filter(({ name }) => name.includes(filterByName.name)));
+  }, [data, filterByName]);
+
+  useEffect(() => {
+    if (filterByNumericValues.length > 0) {
+      const bla = data.filter((planet) => filterByNumericValues.forEach((item) => {
+          if (item.comparison === 'maior que') {
+            return Number(planet[item.column]) > Number(item.value);
+          }
+
+          if (filterByNumericValues[0].comparison === 'menor que') {
+            return Number(planet[item.column]) < Number(item.value);
+          }
+
+          if (item.comparison === 'igual a') {
+            return Number(planet[item.column]) === Number(item.value);
+          }
+        }));
+      console.log(bla);
+    }
+  }, [data, filteredData, filterByNumericValues]); // Lógica para a filtragem pelas colunas com números
+
+  //   if (filterByNumericValues[0].comparison === 'maior que') {
+  //     return Number(planet[filterByNumericValues[0].column]) > Number(filterByNumericValues[0].value);
+  //   }
+  //   if (filterByNumericValues[0].comparison === 'menor que') {
+  //     return Number(planet[filterByNumericValues[0].column]) < Number(filterByNumericValues[0].value);
+  //   }
+  //   if (filterByNumericValues[0].comparison === 'igual a') {
+  //     return Number(planet[filterByNumericValues[0].column]) === Number(filterByNumericValues[0].value);
+  //   }
 
   const filterPlanetByName = ({ target }) => {
     setFilterByName({ name: target.value });
@@ -47,19 +82,19 @@ function App() {
     <>
       <h1>Star Wars</h1>
       {
-        !data
-          ? <img src={ Loading } alt="loading..." />
+        data.length === 0
+          ? <img src={Loading} alt="loading..." />
           : (
             <Context.Provider
-              value={ {
-                data,
+              value={{
+                filteredData,
                 filterByName,
                 filterPlanetByName,
                 numericFilterParameters,
                 getParameters,
                 filterByNumericValues,
                 filterPlanetByNumbers,
-              } }
+              }}
             >
               <FilterByName />
               <FilterByNumber />
--- src/components/Table.js
+++ src/components/Table.js
@@ -3,44 +3,42 @@ import Context from '../context/Context';
 import TableHeader from './TableHeader';
 
 function Table() {
-  const { data, filterByName } = useContext(Context);
+  const { filteredData } = useContext(Context);
 
   return (
     <table>
       <TableHeader />
       <tbody>
-        {data.filter(({ name }) => name
-          .includes(filterByName.name))
-          .map(({
-            name,
-            rotation_period: rotationPeriod,
-            orbital_period: orbitalPeriod,
-            diameter,
-            climate,
-            gravity,
-            terrain,
-            surface_water: surfaceWater,
-            population,
-            films,
-            created,
-            edited,
-            url },
-          index) => (
-            <tr key={ index }>
-              <td>{name}</td>
-              <td>{rotationPeriod}</td>
-              <td>{orbitalPeriod}</td>
-              <td>{diameter}</td>
-              <td>{climate}</td>
-              <td>{gravity}</td>
-              <td>{terrain}</td>
-              <td>{surfaceWater}</td>
-              <td>{population}</td>
-              <td>{films}</td>
-              <td>{created}</td>
-              <td>{edited}</td>
-              <td>{url}</td>
-            </tr>))}
+        {filteredData.map(({
+          name,
+          rotation_period: rotationPeriod,
+          orbital_period: orbitalPeriod,
+          diameter,
+          climate,
+          gravity,
+          terrain,
+          surface_water: surfaceWater,
+          population,
+          films,
+          created,
+          edited,
+          url },
+        index) => (
+          <tr key={ index }>
+            <td>{name}</td>
+            <td>{rotationPeriod}</td>
+            <td>{orbitalPeriod}</td>
+            <td>{diameter}</td>
+            <td>{climate}</td>
+            <td>{gravity}</td>
+            <td>{terrain}</td>
+            <td>{surfaceWater}</td>
+            <td>{population}</td>
+            <td>{films}</td>
+            <td>{created}</td>
+            <td>{edited}</td>
+            <td>{url}</td>
+          </tr>))}
       </tbody>
     </table>
   );
